2cf00974db270ee540549dd6ecf6a4336bec6ed8
ahmad ali <a.0972ali@gmail.com>
2024-03-14 11:56:35 +0300

    Paramiko

--- Learning_Python.py
+++ Learning_Python.py
@@ -1,1 +1,6 @@
-clsss Router:
+class Router :
+    '''Router Class'''
+    def __init__(self , model, swversion, ip_add) :
+    self.model = model
+    self.swversion = swversion
+    self.ip_add = ip_add
